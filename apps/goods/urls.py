ba4b6753be0e401e8087e0dc5d5c81d194eddc5e
Icemelon99 <379510289@qq.com>
2019-05-14 16:10:56 +0800

    商品列表页的显示和排序及页码控制

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, re_path
-from goods.views import IndexView, DetailView
+from django.conf.urls import url
+from goods.views import IndexView, DetailView, ListView
 
 urlpatterns = [
 	path('index/', IndexView.as_view(), name='index'),
-	re_path(r'goods/(?P<goods_id>[0-9]+)', DetailView.as_view(), name='detail'),
+	re_path(r'goods/(?P<goods_id>[0-9]+)$', DetailView.as_view(), name='detail'),
+	re_path(r'list/(?P<type_id>[0-9]+)/(?P<page>[0-9]+)', ListView.as_view(), name='list'),
+
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.urls import reverse
 from django.views import View
 from django.core.cache import cache
 from goods.models import GoodsType, GoodsSKU, GoodsSPU, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 from order.models import OrderGoods
 from django_redis import get_redis_connection
+from django.core.paginator import Paginator
+from django.template import loader
 
 
 
@@ -109,3 +111,82 @@ class DetailView(View):
 		}
 
 		return render(request, 'detail.html', context)
+
+# 需传入的信息有种类，页码，排序方式
+# /list/种类ID/页码?sort=排序方式
+class ListView(View):
+	'''列表页'''
+	def get(self, request, type_id, page):
+		try:
+			# 获取当前分类
+			type_current = GoodsType.objects.get(id=type_id)
+		except GoodsType.DoesNotExist:
+			# 种类不存在
+			return redirect(reverse('goods:index'))
+		# 获取商品的分类信息
+		types = GoodsType.objects.all()
+		# 获取排序的方式及当前分类的商品
+		# sort=default默认排序
+		# sort=price价格排序
+		# sort=hot人气/销量排序 
+		sort = request.GET.get('sort')
+		if sort == 'hot':
+			skus = GoodsSKU.objects.filter(types=type_current).order_by('-sales')
+		elif sort == 'price':
+			skus = GoodsSKU.objects.filter(types=type_current).order_by('price')
+		else:
+			sort = 'default'
+			skus = GoodsSKU.objects.filter(types=type_current).order_by('-id')
+
+		# 对数据进行分页
+		paginator = Paginator(skus, 1)
+		# 获取要求页码的内容
+		try:
+			page = int(page)
+		except Exception as e:
+			page = 1
+		# 判断页码是否超出
+		if page > paginator.num_pages:
+			page = 1
+
+		# 获取指定页码的内容
+		sku_page = paginator.page(page)
+		# 至多显示5个页码，显示当前页的前两页和后两页
+		# 1.页面小于5页，页面上显示所有页码
+		# 2.当前页是前3页，显示1-5页
+		# 3.当前页是后3页，显示后5页
+		# 4.其余：显示当前页的前两页和后两页
+		# 5.添加跳转到第几页和最后一页的按钮，后续实现
+		num_pages = paginator.num_pages
+		if num_pages <= 5:
+			pages = range(1, num_pages+1)
+		elif page <= 3:
+			pages = range(1, 6)
+		elif num_pages-page <= 2:
+			pages = range(num_pages-4, num_pages+1)
+		else:
+			pages = range(page-2, page+3)
+
+		# 获取该种类新品，通过创建时间排序，并切片取前两个
+		new_skus = GoodsSKU.objects.filter(types=type_current).order_by('-create_time')[:2]
+
+		# 获取用户购物车中商品数量
+		user = request.user
+		cart_count = 0
+		# 判断用户已登录
+		if user.is_authenticated:
+			conn = get_redis_connection('default')
+			cart_key = 'cart_{}'.format(user.id)
+			cart_count = conn.hlen(cart_key)
+		# 此参数用于改变页码时不改变排序方式，设计url
+		context = {
+			'type_current': type_current,
+			'types': types, 'skus': skus,
+			'new_skus': new_skus,
+			'cart_count': cart_count,
+			'sort': sort, 
+			'skus_page': sku_page,
+			'pages': pages,
+		}
+		return render(request, 'list.html', context)
+
