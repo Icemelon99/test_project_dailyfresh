1d606bc9e3ad2636808bdbae7aa43186d782f334
Icemelon99 <379510289@qq.com>
2019-05-16 15:40:54 +0800

    使用ajax post添加到购物车，显示购物车页面

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path
+from cart.views import CartAddView, CartInfoView
 
 urlpatterns = [
-	
+	path('add/', CartAddView.as_view(), name='add'),
+	path('info/', CartInfoView.as_view(), name='info'),
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,3 +1,86 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.urls import reverse
+from django.views import View
+from django.urls import reverse
+from django.http import JsonResponse
+from django_redis import get_redis_connection
+from goods.models import GoodsSKU
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+# 前端采用ajax post传递参数，传递商品id(sku_id),商品数量(count)，并接收Json对象
+class CartAddView(View):
+	'''添加购物车记录'''
+	def post(self, request):
+		# 判断用户是否登录
+		user = request.user
+		if not user.is_authenticated:
+			return JsonResponse({'res':0, 'errmsg':'请先登录'})
+
+		# 接收数据，用post方法因此不在定义函数中以形参出现
+		sku_id = request.POST.get('sku_id')
+		count = request.POST.get('count')
+
+		# 校验数据合法性
+		if not all([sku_id, count]):
+			return JsonResponse({'res':1, 'errmsg':'数据不完整'})
+		# 校验商品数量合法性
+		try:
+			count = int(count)
+		except Exception:
+			return JsonResponse({'res':2, 'errmsg':'数目出错'})
+		# 校验商品是否存在
+		try:
+			sku = GoodsSKU.objects.get(id=sku_id)
+		except GoodsSKU.DoesNotExist:
+			return JsonResponse({'res':3, 'errmsg':'商品不存在'})
+		# 添加到购物车，链接到redis数据库
+		conn = get_redis_connection('default')
+		cart_key = 'cart_{}'.format(user.id)
+		# 检索该商品是否已存在，若sku_id在此hash中不存在则返回None
+		cart_count = conn.hget(cart_key, sku_id)
+		if cart_count:
+			count += int(cart_count)
+		# 校验库存值，设置对应值
+		if count>sku.stock:
+			return JsonResponse({'res':4, 'errmsg':'商品库存不足'})
+		conn.hset(cart_key, sku_id, count)
+
+		# 计算用户当前购物车条目数
+		total_count = conn.hlen(cart_key)
+
+		return JsonResponse({'res':5, 'message':'添加成功', 'total_count':total_count})
+
+
+class CartInfoView(LoginRequiredMixin, View):
+	'''购物车页面显示'''
+	def get(self, request):
+		# 获取用户购物车中商品的信息
+		user = request.user
+		conn = get_redis_connection('default')
+		cart_key = 'cart_{}'.format(user.id)
+		# 从数据库中获取购物车字典，{商品id1:数量, 商品id2:数量}
+		cart_dict = conn.hgetall(cart_key)
+		skus = list()
+		total_count = 0
+		total_price = 0
+		for sku_id, count in cart_dict.items():
+			# 根据商品id获取商品信息
+			sku = GoodsSKU.objects.get(id=sku_id)
+			# 计算商品的小计价格
+			amount = sku.price*int(count)
+			# 动态为sku增加属性,小计和数量,并添加到列表中
+			sku.amount = amount
+			sku.count = int(count)
+			skus.append(sku)
+			total_count += int(count)
+			total_price += amount
+		# 组织模板上下文
+		context = {
+			'skus': skus,
+			'total_price': total_price,
+			'total_count': total_count,
+		}
+
+		return render(request, 'cart.html', context)
+
